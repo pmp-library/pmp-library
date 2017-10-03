a93cb1bb5e1deb66f320f01824d2d70618dff49f
Mario Botsch <botsch@techfak.uni-bielefeld.de>
2017-10-03 18:41:39 +0200

    add imgui library

---
+++ external/imgui/imgui_glfw.h
@@ -0,0 +1,27 @@
+// ImGui GLFW binding with OpenGL3 + shaders
+// In this binding, ImTextureID is used to store an OpenGL 'GLuint' texture identifier. Read the FAQ about ImTextureID in imgui.cpp.
+
+// You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+
+#include "imgui.h"
+
+struct GLFWwindow;
+
+IMGUI_API bool        ImGui_Init(GLFWwindow* window, bool install_callbacks);
+IMGUI_API void        ImGui_Shutdown();
+IMGUI_API void        ImGui_NewFrame();
+
+// Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_CreateDeviceObjects();
+
+// GLFW callbacks (installed by default if you enable 'install_callbacks' during initialization)
+// Provided here if you want to chain callbacks.
+// You can also handle inputs yourself and use those as a reference.
+IMGUI_API void        ImGui_MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+IMGUI_API void        ImGui_ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+IMGUI_API void        ImGui_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+IMGUI_API void        ImGui_CharCallback(GLFWwindow* window, unsigned int c);
