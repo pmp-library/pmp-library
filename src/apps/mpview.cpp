8ddf02723e45661477de3bab4fb17b30a5961642
Mario Botsch <botsch@techfak.uni-bielefeld.de>
2017-10-06 10:37:13 +0200

    added mview and mpview apps

--- src/apps/mpview.cpp
+++ src/apps/mpview.cpp
@@ -1,12 +1,12 @@
 //=============================================================================
 
-#include <pmp/gl/MeshViewer.h>
+#include "MeshProcessingViewer.h"
 
 //=============================================================================
 
 int main(int argc, char **argv)
 {
-    pmp::MeshViewer window("MeshViewer", 800, 600);
+    MeshProcessingViewer window("MeshProcessingViewer", 800, 600);
 
     if (argc == 2)
         window.loadMesh(argv[1]);
