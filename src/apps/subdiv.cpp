7fb6637d12e2d5374905f63467d156a5592cd324
Mario Botsch <botsch@techfak.uni-bielefeld.de>
2017-12-16 16:57:22 +0100

    improved subdivision demo

--- src/apps/subdiv.cpp
+++ src/apps/subdiv.cpp
@@ -2,6 +2,8 @@
 
 #include <pmp/gl/MeshViewer.h>
 #include <pmp/algorithms/SurfaceSubdivision.h>
+#include <pmp/algorithms/SurfaceFeatures.h>
+#include <imgui.h>
 
 using namespace pmp;
 
@@ -10,36 +12,68 @@ using namespace pmp;
 class Viewer : public MeshViewer
 {
 public:
-    Viewer(const char* title, int width, int height, bool showgui)
-        : MeshViewer(title, width, height, showgui)
-    {
-        setDrawMode("Hidden Line");
-    }
-
+    Viewer(const char* title, int width, int height, bool showgui=true);
 protected:
+    virtual void processImGUI();
+};
 
-    void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods)
+//=============================================================================
+
+Viewer::Viewer(const char* title, int width, int height, bool showgui)
+    : MeshViewer(title, width, height, showgui)
+{
+    setDrawMode("Hidden Line");
+}
+
+//=============================================================================
+
+void Viewer::processImGUI()
+{
+    MeshViewer::processImGUI();
+
+    ImGui::Spacing();
+    ImGui::Spacing();
+
+    if (ImGui::CollapsingHeader("Subdivision", ImGuiTreeNodeFlags_DefaultOpen))
     {
-        if (action != GLFW_PRESS) return;
-        switch (key)
-        {
-            case GLFW_KEY_ENTER:
-            {
-                if (m_mesh.isTriangleMesh())
-                    SurfaceSubdivision(m_mesh).loop();
-                else
-                    SurfaceSubdivision(m_mesh).catmullClark();
-                updateMesh();
-                break;
-            }
+        //static int featureAngle = 70;
+        //ImGui::PushItemWidth(80);
+        //ImGui::SliderInt("", &featureAngle, 1, 180);
+        //ImGui::PopItemWidth();
+        //ImGui::SameLine();
+        //if (ImGui::Button("Detect Features"))
+        //{
+            //SurfaceFeatures sf(m_mesh);
+            //sf.clear();
+            //sf.detectAngle(featureAngle);
+            //updateMesh();
+        //}
 
-            default:
-            {
-                MeshViewer::keyboard(window, key, scancode, action, mods);
-            }
+        if (ImGui::Button("Triangulate Mesh"))
+        {
+            m_mesh.triangulate();
+            updateMesh();
+        }
+
+        if (ImGui::Button("Loop Subdivision"))
+        {
+            SurfaceSubdivision(m_mesh).loop();
+            updateMesh();
+        }
+
+        //if (ImGui::Button("Sqrt(3) Subdivision"))
+        //{
+            //SurfaceSubdivision(m_mesh).sqrt3();
+            //updateMesh();
+        //}
+
+        if (ImGui::Button("Catmull-Clark Subdivision"))
+        {
+            SurfaceSubdivision(m_mesh).catmullClark();
+            updateMesh();
         }
     }
-};
+}
 
 //=============================================================================
 
@@ -50,7 +84,7 @@ int main(int argc, char **argv)
     if (argc == 2) window.loadMesh(argv[1]);
     return window.run();
 #else
-    Viewer window("Subdivision", 800, 600, false);
+    Viewer window("Subdivision", 800, 600);
     window.loadMesh(argc==2 ? argv[1] : "input.obj");
     return window.run();
 #endif
