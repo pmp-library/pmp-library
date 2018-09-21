68666b0b4449cfc213a5b09ab0f80782707f7441
Daniel Sieger <dsieger@posteo.de>
2018-09-21 22:35:16 +0200

    remove obsolete "All rights reserved" phrase

--- src/pmp/io/SurfaceMeshIO.h
+++ src/pmp/io/SurfaceMeshIO.h
@@ -1,7 +1,6 @@
 //=============================================================================
 // Copyright (C) 2011-2018 The pmp-library developers
 // Copyright (C) 2001-2005 by Computer Graphics Group, RWTH Aachen
-// All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
