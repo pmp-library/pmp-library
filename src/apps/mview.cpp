009d97c0824ad20bc7fb3ae5ca40c9ab49a3bb74
Mario Botsch <botsch@techfak.uni-bielefeld.de>
2019-07-15 22:39:00 +0200

    remove default mesh from emscripten version of mview, to make the app smaller

--- src/apps/mview.cpp
+++ src/apps/mview.cpp
@@ -24,8 +24,6 @@ int main(int argc, char **argv)
     pmp::MeshViewer viewer("MeshViewer", 800, 600, false);
     if (argc > 1)
         viewer.load_mesh(argv[1]);
-    else
-        viewer.load_mesh("input.off");
     if (argc > 2)
         viewer.load_texture(argv[2], GL_SRGB8);
     return viewer.run();
