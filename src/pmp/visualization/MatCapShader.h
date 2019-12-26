7af1a8a8ff008164cc9c0241bb504ee98b0b78e6
Mario Botsch <botsch@techfak.uni-bielefeld.de>
2019-12-26 14:21:39 +0100

    use raw string literals for shader code

--- src/pmp/visualization/MatCapShader.h
+++ src/pmp/visualization/MatCapShader.h
@@ -13,46 +13,40 @@
 // spherical environment mapping is just the normal's XY
 // scaled by 0.5 and shifted by 0.5.
 // scale by 0.49 to avoid artifacts at gracing angles
-static const char* matcap_vshader =
-#ifndef __EMSCRIPTEN__
-    "#version 330\n"
-#else
-    "#version 300 es\n"
-#endif
-    "\n"
-    "layout (location=0) in vec4 v_position;\n"
-    "layout (location=1) in vec3 v_normal;\n"
-    "out vec2 v2f_texcoord;\n"
-    "uniform mat4 modelview_projection_matrix;\n"
-    "uniform mat3 normal_matrix;\n"
-    "\n"
-    "void main()\n"
-    "{\n"
-    "   vec3 n = normalize(normal_matrix * v_normal);\n"
-    "   v2f_texcoord = 0.49 * n.xy + 0.5;\n"
-    "   gl_Position = modelview_projection_matrix * v_position;\n"
-    "}\n";
+static const char* matcap_vshader = R"glsl(
+#version 300 es
+
+layout (location=0) in vec4 v_position;
+layout (location=1) in vec3 v_normal;
+out vec2 v2f_texcoord;
+uniform mat4 modelview_projection_matrix;
+uniform mat3 normal_matrix;
+
+void main()
+{
+    vec3 n = normalize(normal_matrix * v_normal);
+    v2f_texcoord = 0.49 * n.xy + 0.5;
+    gl_Position = modelview_projection_matrix * v_position;
+};
+)glsl";
 
 
-static const char* matcap_fshader =
-#ifndef __EMSCRIPTEN__
-    "#version 330\n"
-#else
-    "#version 300 es\n"
-    "precision mediump float;\n"
-#endif
-    "\n"
-    "in vec2 v2f_texcoord;\n"
-    "uniform sampler2D matcap;\n"
-    "uniform float  alpha;\n"
-    "out vec4 f_color;\n"
-    "\n"
-    "void main()\n"
-    "{\n"
-    "    vec4 rgba = texture(matcap, v2f_texcoord.xy);\n"
-    "    rgba.a *= alpha;\n"
-    "    f_color = rgba;\n"
-    "}";
+static const char* matcap_fshader = R"glsl(
+#version 300 es
+precision mediump float;
+
+in vec2 v2f_texcoord;
+uniform sampler2D matcap;
+uniform float  alpha;
+out vec4 f_color;
+
+void main()
+{
+    vec4 rgba = texture(matcap, v2f_texcoord.xy);
+    rgba.a *= alpha;
+    f_color = rgba;
+};
+)glsl";
 
 
 //=============================================================================
