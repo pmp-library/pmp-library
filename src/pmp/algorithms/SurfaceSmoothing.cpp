d2934d8778da505f42e216a7ea9015b79737d59a
Daniel Sieger <dsieger@posteo.de>
2020-03-22 16:37:34 +0100

    Document exception being thrown

--- src/pmp/algorithms/SurfaceSmoothing.cpp
+++ src/pmp/algorithms/SurfaceSmoothing.cpp
@@ -234,8 +234,8 @@ void SurfaceSmoothing::implicit_smoothing(Scalar timestep,
     Eigen::MatrixXd X = solver.solve(B);
     if (solver.info() != Eigen::Success)
     {
-        throw SolverException(
-            "SurfaceSmoothing: Failed to solve linear system.");
+        auto what = "SurfaceSmoothing: Failed to solve linear system.";
+        throw SolverException(what);
     }
     else
     {
--- src/pmp/algorithms/SurfaceSmoothing.h
+++ src/pmp/algorithms/SurfaceSmoothing.h
@@ -38,9 +38,11 @@ public:
     void explicit_smoothing(unsigned int iters = 10,
                             bool use_uniform_laplace = false);
 
-    //! Perform implicit Laplacian smoothing with \p timestep.
-    //! Decide whether to use uniform Laplacian or cotan Laplacian (default: cotan).
-    //! Decide whether to re-center and re-scale model after smoothing (default: true).
+    //! \brief Perform implicit Laplacian smoothing.
+    //! \param timestep The time step taken.
+    //! \param use_uniform_laplace Decide whether to use uniform Laplacian or cotan Laplacian. Default: cotan.
+    //! \param rescale Decide whether to re-center and re-scale model after smoothing. Default: true.
+    //! \throw SolverException in case of a failure to solve the linear system.
     void implicit_smoothing(Scalar timestep = 0.001,
                             bool use_uniform_laplace = false,
                             bool rescale = true);
