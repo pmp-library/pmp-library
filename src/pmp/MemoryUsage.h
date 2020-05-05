bc02f1358a67074d4f7761d82488f0548542c72b
Daniel Sieger <dsieger@posteo.de>
2020-05-05 23:11:38 +0200

    Do not expose MemoryUsage implementation in header

--- src/pmp/MemoryUsage.h
+++ src/pmp/MemoryUsage.h
@@ -1,26 +1,14 @@
-// Copyright 2017-2019 the Polygon Mesh Processing Library developers.
+// Copyright 2017-2020 the Polygon Mesh Processing Library developers.
 // Distributed under a MIT-style license, see LICENSE.txt for details.
 
 #pragma once
 
-#if defined _WIN32
-#include <psapi.h>
-#include <windows.h>
-#elif defined __linux__
-#include <sys/resource.h>
-#include <unistd.h>
-#include <cstdio>
-#include <iostream>
-#elif defined __APPLE__
-#include <mach/mach.h>
-#endif
+#include <cstddef>
 
 namespace pmp {
 
-//! \addtogroup core core
-//!@{
-
 //! A simple class to retrieve memory usage information.
+//! \ingroup core
 class MemoryUsage
 {
 public:
@@ -32,77 +20,5 @@ public:
     //! \return the current resident set size (RSS) in bytes
     static size_t current_size();
 };
-
-size_t MemoryUsage::max_size()
-{
-#if defined(_WIN32)
-
-    PROCESS_MEMORY_COUNTERS info;
-    GetProcessMemoryInfo(GetCurrentProcess(), &info, sizeof(info));
-    return (size_t)info.PeakWorkingSetSize;
-
-#elif defined(__linux__) || defined(__APPLE__)
-
-    struct rusage rusage;
-    getrusage(RUSAGE_SELF, &rusage);
-
-#if defined(__APPLE__)
-    return (size_t)rusage.ru_maxrss;
-#else
-    return (size_t)(rusage.ru_maxrss * 1024);
-#endif
-
-#endif
-    return 0;
-}
-
-size_t MemoryUsage::current_size()
-{
-#if defined(_WIN32)
-
-    PROCESS_MEMORY_COUNTERS info;
-    GetProcessMemoryInfo(GetCurrentProcess(), &info, sizeof(info));
-    return (size_t)info.WorkingSetSize;
-
-#elif defined(__linux__)
-
-    long rss = 0;
-    FILE* fp = nullptr;
-
-    if ((fp = fopen("/proc/self/statm", "r")) == nullptr)
-    {
-        std::cerr << "Failed to read process information file" << std::endl;
-        return 0;
-    }
-
-    if (fscanf(fp, "%*s%ld", &rss) != 1)
-    {
-        std::cerr << "Failed to retrieve RSS information" << std::endl;
-        fclose(fp);
-        return 0;
-    }
-
-    fclose(fp);
-    return (size_t)rss * (size_t)sysconf(_SC_PAGESIZE);
-
-#elif defined(__APPLE__)
-
-    struct mach_task_basic_info info;
-    mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
-    auto ret = task_info(mach_task_self(), MACH_TASK_BASIC_INFO,
-                         (task_info_t)&info, &infoCount);
-    if (ret != KERN_SUCCESS)
-    {
-        std::cerr << "Failed to retrieve task information" << std::endl;
-        return 0;
-    }
-    return (size_t)info.resident_size;
-
-#else
-    return 0;
-#endif
-}
-
-//!@}
 
 } // namespace pmp
