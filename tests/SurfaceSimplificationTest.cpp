373bb7ff6646721da97aab955c2b02cf7c028ac1
Daniel Sieger <dsieger@posteo.de>
2021-01-15 00:19:22 +0100

    Simplify simplification test

--- tests/SurfaceSimplificationTest.cpp
+++ tests/SurfaceSimplificationTest.cpp
@@ -1,45 +1,34 @@
-// Copyright 2017-2019 the Polygon Mesh Processing Library developers.
+// Copyright 2017-2021 the Polygon Mesh Processing Library developers.
 // Distributed under a MIT-style license, see LICENSE.txt for details.
 
 #include "gtest/gtest.h"
 
-#include <pmp/algorithms/SurfaceSimplification.h>
-#include <pmp/algorithms/SurfaceFeatures.h>
+#include "pmp/algorithms/SurfaceSimplification.h"
+#include "pmp/algorithms/SurfaceFeatures.h"
+#include "Helpers.h"
 
 using namespace pmp;
 
-class SurfaceSimplificationTest : public ::testing::Test
-{
-public:
-    SurfaceSimplificationTest()
-    {
-        EXPECT_TRUE(mesh.read("pmp-data/off/icosahedron_subdiv.off"));
-    }
-    SurfaceMesh mesh;
-};
-
 // plain simplification test
-TEST_F(SurfaceSimplificationTest, simplification)
+TEST(SurfaceSimplificationTest, simplification)
 {
-    mesh.clear();
-    mesh.read("pmp-data/off/bunny_adaptive.off");
+    auto mesh = hemisphere();
     SurfaceSimplification ss(mesh);
-    ss.initialize(5,      // aspect ratio
-                  0.01,   // edge length
-                  10,     // max valence
-                  10,     // normal deviation
-                  0.001); // Hausdorff
+    ss.initialize(5,    // aspect ratio
+                  0.5,  // edge length
+                  10,   // max valence
+                  10,   // normal deviation
+                  0.1); // Hausdorff
     ss.simplify(mesh.n_vertices() * 0.1);
-    EXPECT_EQ(mesh.n_vertices(), size_t(3800));
-    EXPECT_EQ(mesh.n_faces(), size_t(7596));
+    EXPECT_EQ(mesh.n_vertices(), size_t(178));
+    EXPECT_EQ(mesh.n_faces(), size_t(329));
+    mesh.write("simple.off");
 }
 
 // simplify with feature edge preservation enabled
-TEST_F(SurfaceSimplificationTest, simplification_with_features)
+TEST(SurfaceSimplificationTest, simplification_with_features)
 {
-    SurfaceFeatures sf(mesh);
-    sf.detect_angle(25);
-
+    auto mesh = subdivided_icosahedron();
     SurfaceSimplification ss(mesh);
     ss.initialize(5); // aspect ratio
     ss.simplify(mesh.n_vertices() * 0.1);
