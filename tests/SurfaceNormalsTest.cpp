431ec311034932cad373f0c8bb980dd6522b6f23
Daniel Sieger <dsieger@posteo.de>
2021-01-14 23:38:15 +0100

    Simplify normals test

--- tests/SurfaceNormalsTest.cpp
+++ tests/SurfaceNormalsTest.cpp
@@ -1,47 +1,43 @@
-// Copyright 2018-2019 the Polygon Mesh Processing Library developers.
+// Copyright 2018-2021 the Polygon Mesh Processing Library developers.
 // Distributed under a MIT-style license, see LICENSE.txt for details.
 
 #include "gtest/gtest.h"
 
-#include <pmp/algorithms/SurfaceNormals.h>
+#include "pmp/algorithms/SurfaceNormals.h"
+#include "pmp/algorithms/SurfaceFactory.h"
 #include <vector>
 
 using namespace pmp;
 
-class SurfaceNormalsTest : public ::testing::Test
+TEST(SurfaceNormalsTest, compute_vertex_normals)
 {
-public:
-    SurfaceMesh mesh;
-};
-
-TEST_F(SurfaceNormalsTest, compute_vertex_normals)
-{
-    mesh.read("pmp-data/stl/icosahedron_ascii.stl");
+    auto mesh = SurfaceFactory::icosahedron();
     SurfaceNormals::compute_vertex_normals(mesh);
     auto vnormals = mesh.get_vertex_property<Normal>("v:normal");
     auto vn0 = vnormals[Vertex(0)];
     EXPECT_GT(norm(vn0), 0);
 }
 
-TEST_F(SurfaceNormalsTest, compute_face_normals)
+TEST(SurfaceNormalsTest, compute_face_normals)
 {
-    mesh.read("pmp-data/stl/icosahedron_ascii.stl");
+    auto mesh = SurfaceFactory::icosahedron();
     SurfaceNormals::compute_face_normals(mesh);
     auto fnormals = mesh.get_face_property<Normal>("f:normal");
     auto fn0 = fnormals[Face(0)];
     EXPECT_GT(norm(fn0), 0);
 }
 
-TEST_F(SurfaceNormalsTest, compute_corner_normal)
+TEST(SurfaceNormalsTest, compute_corner_normal)
 {
-    mesh.read("pmp-data/stl/icosahedron_ascii.stl");
+    auto mesh = SurfaceFactory::icosahedron();
     auto h = Halfedge(0);
     auto n = SurfaceNormals::compute_corner_normal(mesh, h, (Scalar)M_PI / 3.0);
     EXPECT_GT(norm(n), 0);
 }
 
-TEST_F(SurfaceNormalsTest, polygonal_face_normal)
+TEST(SurfaceNormalsTest, polygonal_face_normal)
 {
+    SurfaceMesh mesh;
     std::vector<Vertex> vertices(5);
     vertices[0] = mesh.add_vertex(Point(0, 0, 0));
     vertices[1] = mesh.add_vertex(Point(1, 0, 0));
