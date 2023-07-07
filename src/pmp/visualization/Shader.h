454f0592a50ec31d6588787ccc8ac3dc6f5d320a
Daniel Sieger <dsieger@posteo.de>
2023-07-07 14:46:54 +0200

    Doxygen

--- src/pmp/visualization/Shader.h
+++ src/pmp/visualization/Shader.h
@@ -15,20 +15,20 @@ namespace pmp {
 class Shader
 {
 public:
-    //! default destructor
+    //! Default destructor, clears all shaders.
     ~Shader();
 
-    //! is shader valid (ID != 0)
+    //! Indicate if shader is valid.
     bool is_valid() const { return pid_ != 0; }
 
-    //! get source from strings, compile, and link vertex and fragment shader,
+    //! Get source from strings, compile, and link vertex and fragment shader.
     //! \param vshader string with the address to the vertex shader
     //! \param fshader string with the address to the fragment shader
     void source(const char* vshader, const char* fshader);
 
-    //! load (from file), compile, and link vertex and fragment shader,
+    //! Load from file, compile, and link vertex and fragment shader,
     //! and optional geometry and tessellation shaders.
-    //! unused shaders should be NULL.
+    //! Unused shaders should be nullptr.
     //! \param vfile string with the address to the vertex shader
     //! \param ffile string with the address to the fragment shader
     //! \param gfile filename of geometry shader
@@ -37,41 +37,41 @@ public:
     void load(const char* vfile, const char* ffile, const char* gfile = nullptr,
               const char* tcfile = nullptr, const char* tefile = nullptr);
 
-    //! enable/bind this shader program
+    //! Enable/bind this shader program.
     void use();
 
-    //! disable/unbind this shader program
+    //! Disable/unbind this shader program.
     void disable();
 
-    //! bind attribute to location
+    //! Bind attribute to location.
     void bind_attribute(const char* name, GLuint index);
 
-    //! upload float uniform
+    //! Upload float uniform.
     //! \param name string of the uniform name
     //! \param value the value for the uniform
     void set_uniform(const char* name, float value);
 
-    //! upload int uniform
+    //! Upload int uniform.
     //! \param name string of the uniform name
     //! \param value the value for the uniform
     void set_uniform(const char* name, int value);
 
-    //! upload vec3 uniform
+    //! Upload vec3 uniform.
     //! \param name string of the uniform name
     //! \param vec the value for the uniform
     void set_uniform(const char* name, const vec3& vec);
 
-    //! upload vec4 uniform
+    //! Upload vec4 uniform.
     //! \param name string of the uniform name
     //! \param vec the value for the uniform
     void set_uniform(const char* name, const vec4& vec);
 
-    //! upload mat3 uniform
+    //! Upload mat3 uniform.
     //! \param name string of the uniform name
     //! \param mat the value for the uniform
     void set_uniform(const char* name, const mat3& mat);
 
-    //! upload mat4 uniform
+    //! Upload mat4 uniform.
     //! \param name string of the uniform name
     //! \param mat the value for the uniform
     void set_uniform(const char* name, const mat4& mat);
