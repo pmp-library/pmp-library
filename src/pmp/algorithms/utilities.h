98e55fcd02c78dc9127d5acf63ddc7b0627456c7
Daniel Sieger <dsieger@posteo.de>
2023-07-17 23:44:32 +0200

    Use \addtogroup for grouping

--- src/pmp/algorithms/utilities.h
+++ src/pmp/algorithms/utilities.h
@@ -8,20 +8,19 @@
 
 namespace pmp {
 
+//! \addtogroup algorithms
+//! @{
+
 //! Compute the bounding box of \p mesh .
-//! \ingroup algorithms
 BoundingBox bounds(const SurfaceMesh& mesh);
 
 //! Flip the orientation of all faces in \p mesh .
-//! \ingroup algorithms
 void flip_faces(SurfaceMesh& mesh);
 
 //! Compute the minimum area of all faces in \p mesh .
-//! \ingroup algorithms
 Scalar min_face_area(const SurfaceMesh& mesh);
 
 //! Compute length of an edge \p e in \p mesh .
-//! \ingroup algorithms
 inline Scalar edge_length(const SurfaceMesh& mesh, Edge e)
 {
     return distance(mesh.position(mesh.vertex(e, 0)),
@@ -29,7 +28,8 @@ inline Scalar edge_length(const SurfaceMesh& mesh, Edge e)
 }
 
 //! Compute mean edge length of \p mesh .
-//! \ingroup algorithms
 Scalar mean_edge_length(const SurfaceMesh& mesh);
+
+//! @}
 
 } // namespace pmp
