9f8be19586f8786db7b3d2212d38cce28adaa35f
Daniel Sieger <dsieger@posteo.de>
2023-08-22 11:37:19 +0200

    Expose algorithm parameters in remeshing demo

--- examples/remeshing.cpp
+++ examples/remeshing.cpp
@@ -33,7 +33,7 @@ void Viewer::process_imgui()
     ImGui::Spacing();
     ImGui::Spacing();
 
-    if (ImGui::CollapsingHeader("Remeshing", ImGuiTreeNodeFlags_DefaultOpen))
+    if (ImGui::CollapsingHeader("Features", ImGuiTreeNodeFlags_DefaultOpen))
     {
         static int feature_angle = 70;
         ImGui::PushItemWidth(80);
@@ -46,20 +46,37 @@ void Viewer::process_imgui()
             detect_features(mesh_, feature_angle);
             update_mesh();
         }
+    }
 
-        ImGui::Text("Remeshing:");
+    if (ImGui::CollapsingHeader("Uniform Remeshing",
+                                ImGuiTreeNodeFlags_DefaultOpen))
+    {
+        ImGui::PushItemWidth(80);
+
+        static double edge_length{0.01};
+        ImGui::InputDouble("Edge Length", &edge_length, 0, 0, "%g");
         ImGui::SameLine();
-
-        if (ImGui::Button("Uniform"))
+        if (ImGui::Button("Mean"))
         {
-            Scalar l(0);
-            for (auto eit : mesh_.edges())
-                l += distance(mesh_.position(mesh_.vertex(eit, 0)),
-                              mesh_.position(mesh_.vertex(eit, 1)));
-            l /= (Scalar)mesh_.n_edges();
+            edge_length = mean_edge_length(mesh_);
+        }
+
+        static int n_iterations{10};
+        ImGui::SliderInt("Iterations##uniform", &n_iterations, 1, 20);
+
+        static bool use_projection{true};
+        ImGui::Checkbox("Use Projection##uniform", &use_projection);
+
+        static bool scale_lengths{true};
+        ImGui::Checkbox("Scale Lengths##uniform", &scale_lengths);
+
+        if (ImGui::Button("Remesh##uniform"))
+        {
             try
             {
-                uniform_remeshing(mesh_, l);
+                auto scaling = scale_lengths ? bounds(mesh_).size() : 1.0;
+                uniform_remeshing(mesh_, edge_length * scaling, n_iterations,
+                                  use_projection);
             }
             catch (const InvalidInputException& e)
             {
@@ -69,17 +86,40 @@ void Viewer::process_imgui()
             update_mesh();
         }
 
-        ImGui::SameLine();
+        ImGui::PopItemWidth();
+    }
 
-        if (ImGui::Button("Adaptive"))
+    if (ImGui::CollapsingHeader("Adaptive Remeshing",
+                                ImGuiTreeNodeFlags_DefaultOpen))
+    {
+        ImGui::PushItemWidth(80);
+
+        static double min_length{0.001};
+        ImGui::InputDouble("Min. Edge Length", &min_length, 0, 0, "%g");
+
+        static double max_length{0.05};
+        ImGui::InputDouble("Max. Edge Length", &max_length, 0, 0, "%g");
+
+        static double max_error{0.0005};
+        ImGui::InputDouble("Max. Error", &max_error, 0, 0, "%g");
+
+        static int n_iterations{10};
+        ImGui::SliderInt("Iterations##adaptive", &n_iterations, 1, 20);
+
+        static bool use_projection{true};
+        ImGui::Checkbox("Use Projection##adaptive", &use_projection);
+
+        static bool scale_lengths{true};
+        ImGui::Checkbox("Scale Lengths##adaptive", &scale_lengths);
+
+        if (ImGui::Button("Remesh##adaptive"))
         {
-            auto bb = bounds(mesh_).size();
+            auto scaling = scale_lengths ? bounds(mesh_).size() : 1.0;
             try
             {
-                adaptive_remeshing(mesh_,
-                                   0.0010 * bb,  // min length
-                                   0.0500 * bb,  // max length
-                                   0.0005 * bb); // approx. error
+                adaptive_remeshing(mesh_, min_length * scaling,
+                                   max_length * scaling, max_error * scaling,
+                                   n_iterations, use_projection);
             }
             catch (const InvalidInputException& e)
             {
@@ -88,6 +128,8 @@ void Viewer::process_imgui()
             }
             update_mesh();
         }
+
+        ImGui::PopItemWidth();
     }
 }
 
