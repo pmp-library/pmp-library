e19be705b09713aba7697c18b3389036aba677cc
Daniel Sieger <dsieger@posteo.de>
2024-03-24 18:39:03 +0100

    Add hole filling regression test

--- tests/regression_test.cpp
+++ tests/regression_test.cpp
@@ -4,6 +4,7 @@
 #include "gtest/gtest.h"
 
 #include "pmp/algorithms/hole_filling.h"
+#include "pmp/algorithms/shapes.h"
 #include "pmp/algorithms/subdivision.h"
 #include "pmp/algorithms/triangulation.h"
 #include "pmp/algorithms/decimation.h"
@@ -37,6 +38,7 @@ Halfedge find_boundary(const SurfaceMesh& mesh)
 };
 } // namespace
 
+// run essential algorithms that heavily modify topology and geometry
 TEST(RegressionTest, bunny)
 {
     SurfaceMesh mesh;
@@ -72,5 +74,44 @@ TEST(RegressionTest, bunny)
     auto h = find_boundary(mesh);
     fill_hole(mesh, h);
 
+    // no more holes
+    EXPECT_FALSE(find_boundary(mesh).is_valid());
+}
+
+// benchmark hole filling on a mesh with lots of holes
+TEST(RegressionTest, hole_filling)
+{
+    // take a sphere
+    auto mesh = icosphere(2);
+
+    // select all initial vertices
+    auto selected = mesh.add_vertex_property<bool>("v:selected");
+    for (const auto v : mesh.vertices())
+        selected[v] = true;
+
+    // refine
+    for (int i = 0; i < 4; i++)
+        loop_subdivision(mesh);
+
+    // grow original selection
+    for (int i = 0; i < 2; i++)
+        grow_selection(mesh);
+
+    // delete selected
+    for (const auto v : mesh.vertices())
+        if (selected[v])
+            mesh.delete_vertex(v);
+    mesh.garbage_collection();
+
+    // fill all holes
+    while (true)
+    {
+        const auto h = find_boundary(mesh);
+        if (!h.is_valid())
+            break;
+        fill_hole(mesh, h);
+    }
+
+    // no more holes
     EXPECT_FALSE(find_boundary(mesh).is_valid());
 }
