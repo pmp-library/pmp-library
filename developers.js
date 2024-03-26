0c947adf1e2878eeb6e747c8152bc7159ce3fe87
dsieger <dsieger@users.noreply.github.com>
2024-03-26 21:27:38 +0000

    deploy: b441abd8fde21c965e1a3f9eba2891ab13a1662c

---
+++ developers.js
@@ -0,0 +1,42 @@
+var developers =
+[
+    [ "Contributing", "contributing.html", [
+      [ "Reporting Issues", "contributing.html#autotoc_md32", null ],
+      [ "Contributing Code", "contributing.html#autotoc_md33", null ],
+      [ "Developer Certificate of Origin", "contributing.html#autotoc_md34", null ],
+      [ "Unit Testing", "contributing.html#autotoc_md35", null ],
+      [ "Code Coverage", "contributing.html#autotoc_md36", null ],
+      [ "Static Analysis", "contributing.html#autotoc_md37", null ]
+    ] ],
+    [ "Coding Style", "codingstyle.html", [
+      [ "Naming", "codingstyle.html#autotoc_md8", [
+        [ "Types", "codingstyle.html#autotoc_md9", null ],
+        [ "Functions", "codingstyle.html#autotoc_md10", null ],
+        [ "Variables", "codingstyle.html#autotoc_md11", null ],
+        [ "File Names", "codingstyle.html#autotoc_md12", null ]
+      ] ],
+      [ "Formatting", "codingstyle.html#autotoc_md13", [
+        [ "Blocks", "codingstyle.html#autotoc_md14", null ],
+        [ "Line Length", "codingstyle.html#autotoc_md15", null ],
+        [ "Indentation", "codingstyle.html#autotoc_md16", null ]
+      ] ],
+      [ "Miscellaneous", "codingstyle.html#autotoc_md17", [
+        [ "Declaration Order", "codingstyle.html#autotoc_md18", null ],
+        [ "Comments", "codingstyle.html#autotoc_md19", null ],
+        [ "Doxygen Comments", "codingstyle.html#autotoc_md20", null ],
+        [ "Include Guards", "codingstyle.html#autotoc_md21", null ],
+        [ "Header Include Order", "codingstyle.html#autotoc_md22", null ],
+        [ "Include Style", "codingstyle.html#autotoc_md23", null ],
+        [ "Namespace", "codingstyle.html#autotoc_md24", null ],
+        [ "Boolean Prefixes", "codingstyle.html#autotoc_md25", null ],
+        [ "Naming Consistency", "codingstyle.html#autotoc_md26", null ],
+        [ "Type Consistency", "codingstyle.html#autotoc_md27", null ],
+        [ "Structs vs. Classes", "codingstyle.html#autotoc_md28", null ],
+        [ "Scoping", "codingstyle.html#autotoc_md29", null ],
+        [ "Prefer C++ over C", "codingstyle.html#autotoc_md30", null ]
+      ] ],
+      [ "Using clang-format", "codingstyle.html#using-clang-format", null ],
+      [ "Commit Messages", "codingstyle.html#commit-messages", null ],
+      [ "Further Reading", "codingstyle.html#autotoc_md31", null ]
+    ] ]
+];
---
+++ search/enumvalues_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gauss_0',['gauss',['../group__algorithms.html#ggab0bc5cf882dd9adfbf56f396bde50fa6a710a4950250286365cf841f765a790f1',1,'pmp']]]
+];
