b85e8870a539e231f88de836a286123e5184fe8b
Daniel Sieger <dsieger@posteo.de>
2024-08-17 19:52:12 +0200

    Use const auto

--- src/pmp/algorithms/shapes.cpp
+++ src/pmp/algorithms/shapes.cpp
@@ -159,50 +159,51 @@ SurfaceMesh uv_sphere(const Point& center, Scalar radius, size_t n_slices,
     SurfaceMesh mesh;
 
     // add top vertex
-    auto v0 = mesh.add_vertex(Point(center[0], center[1] + radius, center[2]));
+    const auto top = Point(center[0], center[1] + radius, center[2]);
+    const auto v0 = mesh.add_vertex(top);
 
     // generate vertices per stack / slice
     for (size_t i = 0; i < n_stacks - 1; i++)
     {
-        auto phi = std::numbers::pi * double(i + 1) / double(n_stacks);
+        const auto phi = std::numbers::pi * double(i + 1) / double(n_stacks);
         for (size_t j = 0; j < n_slices; ++j)
         {
-            auto theta = 2.0 * std::numbers::pi * double(j) / double(n_slices);
-            auto x = center[0] + radius * std::sin(phi) * std::cos(theta);
-            auto y = center[1] + radius * std::cos(phi);
-            auto z = center[2] + radius * std::sin(phi) * std::sin(theta);
+            const auto theta =
+                2.0 * std::numbers::pi * double(j) / double(n_slices);
+            const auto x = center[0] + radius * std::sin(phi) * std::cos(theta);
+            const auto y = center[1] + radius * std::cos(phi);
+            const auto z = center[2] + radius * std::sin(phi) * std::sin(theta);
             mesh.add_vertex(Point(x, y, z));
         }
     }
 
     // add bottom vertex
-    auto v1 = mesh.add_vertex(Point(center[0], center[1] - radius, center[2]));
+    const auto bottom = Point(center[0], center[1] - radius, center[2]);
+    const auto v1 = mesh.add_vertex(bottom);
 
     // add top / bottom triangles
-    size_t i0, i1;
     for (size_t i = 0; i < n_slices; ++i)
     {
-        i0 = i + 1;
-        i1 = (i + 1) % n_slices + 1;
+        const auto i0 = i + 1;
+        const auto i1 = (i + 1) % n_slices + 1;
         mesh.add_triangle(v0, Vertex(i1), Vertex(i0));
 
-        i0 = i + n_slices * (n_stacks - 2) + 1;
-        i1 = (i + 1) % n_slices + n_slices * (n_stacks - 2) + 1;
-        mesh.add_triangle(v1, Vertex(i0), Vertex(i1));
+        const auto i2 = i + n_slices * (n_stacks - 2) + 1;
+        const auto i3 = (i + 1) % n_slices + n_slices * (n_stacks - 2) + 1;
+        mesh.add_triangle(v1, Vertex(i2), Vertex(i3));
     }
 
     // add quads per stack / slice
-    size_t i2, i3;
     for (size_t j = 0; j < n_stacks - 2; ++j)
     {
-        size_t idx0 = j * n_slices + 1;
-        size_t idx1 = (j + 1) * n_slices + 1;
+        const auto idx0 = j * n_slices + 1;
+        const auto idx1 = (j + 1) * n_slices + 1;
         for (size_t i = 0; i < n_slices; ++i)
         {
-            i0 = idx0 + i;
-            i1 = idx0 + (i + 1) % n_slices;
-            i2 = idx1 + (i + 1) % n_slices;
-            i3 = idx1 + i;
+            const auto i0 = idx0 + i;
+            const auto i1 = idx0 + (i + 1) % n_slices;
+            const auto i2 = idx1 + (i + 1) % n_slices;
+            const auto i3 = idx1 + i;
             mesh.add_quad(Vertex(i0), Vertex(i1), Vertex(i2), Vertex(i3));
         }
     }
