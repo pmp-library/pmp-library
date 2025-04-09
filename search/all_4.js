e6b4b8ae0fcc9d95a2b5990e32c6dc72ace0f6d9
mbotsch <mbotsch@users.noreply.github.com>
2025-04-09 13:50:30 +0000

    deploy: 63397d8dbb4e692e20a2baa3b1eda700a5ef0454

---
+++ search/all_4.js
@@ -0,0 +1,20 @@
+var searchData=
+[
+  ['edge_0',['edge',['../classpmp_1_1_surface_mesh.html#ad458db917bf85de7ac73bc17808daa14',1,'pmp::SurfaceMesh']]],
+  ['edge_1',['Edge',['../classpmp_1_1_edge.html',1,'pmp']]],
+  ['edge_5farea_2',['edge_area',['../group__algorithms.html#ga48ef675f84e1ee4c021694c1f54d9164',1,'pmp']]],
+  ['edge_5flength_3',['edge_length',['../group__algorithms.html#gaa73faf69000512c8b2d23bba5fe8423e',1,'pmp']]],
+  ['edge_5fproperties_4',['edge_properties',['../classpmp_1_1_surface_mesh.html#a6bd4a8d297474575c2089ccae456d31a',1,'pmp::SurfaceMesh']]],
+  ['edge_5fproperty_5',['edge_property',['../classpmp_1_1_surface_mesh.html#ab72c709f83f75c767b2c48727f2db619',1,'pmp::SurfaceMesh']]],
+  ['edgearoundvertexcirculator_6',['EdgeAroundVertexCirculator',['../classpmp_1_1_surface_mesh_1_1_edge_around_vertex_circulator.html#ada12121a421e0657eddf82e424a0c209',1,'pmp::SurfaceMesh::EdgeAroundVertexCirculator::EdgeAroundVertexCirculator()'],['../classpmp_1_1_surface_mesh_1_1_edge_around_vertex_circulator.html',1,'SurfaceMesh::EdgeAroundVertexCirculator']]],
+  ['edgecontainer_7',['EdgeContainer',['../classpmp_1_1_surface_mesh_1_1_edge_container.html',1,'pmp::SurfaceMesh']]],
+  ['edgeiterator_8',['EdgeIterator',['../classpmp_1_1_surface_mesh_1_1_edge_iterator.html#aa9be6a00b4c1b853034adce5c24f5165',1,'pmp::SurfaceMesh::EdgeIterator::EdgeIterator()'],['../classpmp_1_1_surface_mesh_1_1_edge_iterator.html',1,'SurfaceMesh::EdgeIterator']]],
+  ['edgeproperty_9',['EdgeProperty',['../classpmp_1_1_edge_property.html#a6fbc9dc1a3c40a349d7d221c4e2f7c1b',1,'pmp::EdgeProperty::EdgeProperty()'],['../classpmp_1_1_edge_property.html',1,'EdgeProperty&lt; T &gt;']]],
+  ['edgeproperty_3c_20bool_20_3e_10',['EdgeProperty&lt; bool &gt;',['../classpmp_1_1_edge_property.html',1,'pmp']]],
+  ['edges_11',['edges',['../classpmp_1_1_surface_mesh.html#a0badaef8b3f0fab59125eedb12e5556e',1,'pmp::SurfaceMesh::edges() const'],['../classpmp_1_1_surface_mesh.html#a182905105e2b83d232bbcacf0430b946',1,'pmp::SurfaceMesh::edges(Vertex v) const']]],
+  ['edges_5fbegin_12',['edges_begin',['../classpmp_1_1_surface_mesh.html#a5b369fd79dede60706600e60b4788fc9',1,'pmp::SurfaceMesh']]],
+  ['edges_5fend_13',['edges_end',['../classpmp_1_1_surface_mesh.html#a155a6587f1e778e8a7756b066965369d',1,'pmp::SurfaceMesh']]],
+  ['edges_5fsize_14',['edges_size',['../classpmp_1_1_surface_mesh.html#a60e4af817e3a9bb25ce627275411717a',1,'pmp::SurfaceMesh']]],
+  ['elapsed_15',['elapsed',['../classpmp_1_1_stop_watch.html#a97dc28a21f15bc9e255cd29bcda1ba02',1,'pmp::StopWatch']]],
+  ['explicit_5fsmoothing_16',['explicit_smoothing',['../group__algorithms.html#gabfa24662ed3dcb46496f9da995ebc18d',1,'pmp']]]
+];
---
+++ search/functions_1.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['back_5fcolor_0',['back_color',['../classpmp_1_1_renderer.html#ab689302935c666fa149da7a1d7c89729',1,'pmp::Renderer']]],
+  ['bind_5fattribute_1',['bind_attribute',['../classpmp_1_1_shader.html#a0318d9f8778a08aa2c05a2e21f889190',1,'pmp::Shader']]],
+  ['boundingbox_2',['BoundingBox',['../classpmp_1_1_bounding_box.html#a1dea7b72431cc6891cefdb6020af2bd6',1,'pmp::BoundingBox::BoundingBox()'],['../classpmp_1_1_bounding_box.html#a01744459540644fceba0439aa180b35d',1,'pmp::BoundingBox::BoundingBox(const Point &amp;min, const Point &amp;max)']]],
+  ['bounds_3',['bounds',['../group__algorithms.html#ga0fec476ec9ff00c29ef3f3ae388e30b2',1,'pmp']]]
+];
