78858530de19e18c1c0649b946f959dac3caa602
mbotsch <mbotsch@users.noreply.github.com>
2025-05-17 18:28:56 +0000

    deploy: 5aeba00d0283b8dc3b0c7c608bff59df6094898b

---
+++ search/all_10.js
@@ -0,0 +1,24 @@
+var searchData=
+[
+  ['read_0',['read',['../group__io.html#ga2bcb6c26485632205f195d41d3aa66d6',1,'pmp']]],
+  ['remeshing_1',['Remeshing',['../remeshing.html',1,'algorithms-guide']]],
+  ['remove_5fedge_2',['remove_edge',['../classpmp_1_1_surface_mesh.html#a0438b9632f38f9216e99968de0091dc8',1,'pmp::SurfaceMesh']]],
+  ['remove_5fedge_5fproperty_3',['remove_edge_property',['../classpmp_1_1_surface_mesh.html#a3eeefd3f1bac92af03188fb814b23f4c',1,'pmp::SurfaceMesh']]],
+  ['remove_5fface_5fproperty_4',['remove_face_property',['../classpmp_1_1_surface_mesh.html#a888b18cf5fcb3776ee59708d317bb323',1,'pmp::SurfaceMesh']]],
+  ['remove_5fhalfedge_5fproperty_5',['remove_halfedge_property',['../classpmp_1_1_surface_mesh.html#a738b7b5ee1edeb4f25d331bd8c092337',1,'pmp::SurfaceMesh']]],
+  ['remove_5fvertex_5fproperty_6',['remove_vertex_property',['../classpmp_1_1_surface_mesh.html#a6253cef536ace70bb020cacfd3dee5fa',1,'pmp::SurfaceMesh']]],
+  ['renderer_7',['Renderer',['../classpmp_1_1_renderer.html',1,'Renderer'],['../classpmp_1_1_renderer.html#a4c56aa82aef1e710d074c4e0134acb11',1,'pmp::Renderer::Renderer()']]],
+  ['reserve_8',['reserve',['../classpmp_1_1_surface_mesh.html#a5ba773ee44e3fe291bcc7811e837852a',1,'pmp::SurfaceMesh']]],
+  ['reset_9',['reset',['../classpmp_1_1_handle.html#ad20897c5c8bd47f5d4005989bead0e55',1,'pmp::Handle']]],
+  ['resize_10',['resize',['../classpmp_1_1_window.html#a88ae60d5d0b3a164e396e205b1216dd7',1,'pmp::Window']]],
+  ['resume_11',['resume',['../classpmp_1_1_stop_watch.html#a41de8150eff044a237990c271d57ea27',1,'pmp::StopWatch']]],
+  ['right_5fmouse_5fpressed_12',['right_mouse_pressed',['../classpmp_1_1_window.html#aea3d836a2f8eb0855de0641f345f7f6d',1,'pmp::Window']]],
+  ['rotate_13',['rotate',['../classpmp_1_1_trackball_viewer.html#ac3e26103d7c76d83d4ae385dffabb8f7',1,'pmp::TrackballViewer']]],
+  ['rotation_14',['rotation',['../classpmp_1_1_trackball_viewer.html#a42644844994c260461dc67fcd9de5bba',1,'pmp::TrackballViewer']]],
+  ['rotation_5fmatrix_15',['rotation_matrix',['../group__core.html#gaa7e80d5c42f702802e7f89ed76683722',1,'pmp::rotation_matrix(const Vector&lt; Scalar, 3 &gt; &amp;axis, Scalar angle)'],['../group__core.html#gac477e9ebe7754fd68c0cdcb31782288e',1,'pmp::rotation_matrix(const Vector&lt; Scalar, 4 &gt; &amp;quat)']]],
+  ['rotation_5fmatrix_5fx_16',['rotation_matrix_x',['../group__core.html#ga175c91f9a2d03706ea041e65f5316f0d',1,'pmp']]],
+  ['rotation_5fmatrix_5fy_17',['rotation_matrix_y',['../group__core.html#gacc93661d81716c71c2658328118367c4',1,'pmp']]],
+  ['rotation_5fmatrix_5fz_18',['rotation_matrix_z',['../group__core.html#ga2181fdfa6885121531f27296b2480987',1,'pmp']]],
+  ['rows_19',['rows',['../classpmp_1_1_matrix.html#a36961f23ce2653ac605ebf60f072094e',1,'pmp::Matrix']]],
+  ['run_20',['run',['../classpmp_1_1_window.html#a58e8be2db2660128e4e6456a9c981fb5',1,'pmp::Window']]]
+];
---
+++ search/typedefs_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['point_0',['Point',['../group__core.html#ga4fc38b084567aed726be776958bfcc78',1,'pmp']]]
+];
