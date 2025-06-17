3cd3c2a909331473288418c1e38f85679c6c88ca
mbotsch <mbotsch@users.noreply.github.com>
2025-06-17 10:03:31 +0000

    deploy: adfbc68ce2ecbaf04fa6243ae0314b5774d5f246

---
+++ search/classes_7.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['matrix_0',['Matrix',['../classpmp_1_1_matrix.html',1,'pmp']]],
+  ['matrix_3c_20float_20_3e_1',['Matrix&lt; float &gt;',['../classpmp_1_1_matrix.html',1,'pmp']]],
+  ['matrix_3c_20float_2c_203_20_3e_2',['Matrix&lt; float, 3 &gt;',['../classpmp_1_1_matrix.html',1,'pmp']]],
+  ['matrix_3c_20int_2c_202_20_3e_3',['Matrix&lt; int, 2 &gt;',['../classpmp_1_1_matrix.html',1,'pmp']]],
+  ['matrix_3c_20scalar_2c_203_20_3e_4',['Matrix&lt; Scalar, 3 &gt;',['../classpmp_1_1_matrix.html',1,'pmp']]],
+  ['memoryusage_5',['MemoryUsage',['../classpmp_1_1_memory_usage.html',1,'pmp']]],
+  ['meshviewer_6',['MeshViewer',['../classpmp_1_1_mesh_viewer.html',1,'pmp']]]
+];
---
+++ search/functions_12.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['tetrahedron_0',['tetrahedron',['../group__algorithms.html#ga6f5ecdb5b08657b2898f8d6a0c9157cf',1,'pmp']]],
+  ['to_5fvertex_1',['to_vertex',['../classpmp_1_1_surface_mesh.html#a662504cbe67b5f9fd40b1ffe605d0ac7',1,'pmp::SurfaceMesh']]],
+  ['torus_2',['torus',['../group__algorithms.html#gae7d933cd48c728f9f45cd61c26a40e25',1,'pmp']]],
+  ['trackballviewer_3',['TrackballViewer',['../classpmp_1_1_trackball_viewer.html#a3b3aabe9a92251616debaa753a2742ea',1,'pmp::TrackballViewer']]],
+  ['translate_4',['translate',['../classpmp_1_1_trackball_viewer.html#a1271c902747facdff926e6bd096c21da',1,'pmp::TrackballViewer']]],
+  ['translation_5',['translation',['../classpmp_1_1_trackball_viewer.html#aa9686ad1ec15521b5feb7d67703cab13',1,'pmp::TrackballViewer']]],
+  ['translation_5fmatrix_6',['translation_matrix',['../group__core.html#ga3bac49500526615c2eb3ebc6a0f73f2c',1,'pmp']]],
+  ['transpose_7',['transpose',['../group__core.html#gaa048c4330d5e3280d853a26f9ec63f76',1,'pmp']]],
+  ['triangle_5farea_8',['triangle_area',['../group__algorithms.html#gab25b22a28545fce6bdb9112c22df5a06',1,'pmp']]],
+  ['triangulate_9',['triangulate',['../group__algorithms.html#ga6fb9055ec7d7d8c31e99bbfdfa57785a',1,'pmp::triangulate(SurfaceMesh &amp;mesh)'],['../group__algorithms.html#ga3aa015ca35277318e89bd3d2d3e76bc4',1,'pmp::triangulate(SurfaceMesh &amp;mesh, Face f)']]]
+];
