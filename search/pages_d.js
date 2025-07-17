53f1e7a5d2e1c693c358ba4ed899ccf9c01da457
mbotsch <mbotsch@users.noreply.github.com>
2025-07-17 14:32:28 +0000

    deploy: 7e96ebf7197e1f141afcf59755269884cdcb1e04

---
+++ search/pages_d.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['version_201_2e0_20released_0',['Version 1.0 Released',['../version-one-released-2019-02-18.html',1,'news']]],
+  ['version_201_2e1_20released_1',['Version 1.1 Released',['../version-1-1-released-2019-05-30.html',1,'news']]],
+  ['version_201_2e2_20released_2',['Version 1.2 Released',['../version-1-2-released-2020-03-15.html',1,'news']]],
+  ['version_201_2e2_2e1_20released_3',['Version 1.2.1 Released',['../version-1-2-1-released-2020-05-10.html',1,'news']]],
+  ['version_202_2e0_20released_4',['Version 2.0 Released',['../version-2-0-released-2022-08-14.html',1,'news']]],
+  ['version_202_2e0_2e1_20released_5',['Version 2.0.1 Released',['../version-2-0-1-released-2022-08-26.html',1,'news']]],
+  ['version_203_2e0_20released_6',['Version 3.0 Released',['../version-3-0-released-2023-08-24.html',1,'news']]]
+];
