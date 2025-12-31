e2ebf5c606c4a37fdfb8196e0c3db03efccaf653
Tadeáš Němec <tadeas.nemec@icloud.com>
2025-12-31 11:46:50 +0100

    Add IO module for mesh Read/Write.
    Signed-off-by: Tadeáš Němec <72395968+nemectad@users.noreply.github.com>
    Signed-off-by: Tadeáš Němec <tadeas.nemec@icloud.com>

---
+++ src/pmp/python/include/io.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/operators.h>
+#include <sstream>
+
+#include "pmp/surface_mesh.h"
+#include "pmp/io/io.h"
+#include "pmp/io/io_flags.h"
+
+namespace py = pybind11;
+void bind_io(py::module_ &io);
